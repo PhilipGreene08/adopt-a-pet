bf85171133cd00467dc27c9908b42d5ca8c06b09
Philip Grene <54162361+PhilipGreene08@users.noreply.github.com>
2021-08-08 21:46:32 -0500

    Add files via upload

--- animal-finder/script.js
+++ animal-finder/script.js
@@ -10,10 +10,47 @@ const zip = document.getElementById('zip')
 const distance = document.getElementById('distance')
 
 //check for type
-// submit.addEventListener('click', function (event) {
-//     event.preventDefault()
 
-//     if (type.value === 'none' | distance.value > 500) {
+submit.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    let animalData
+    if (type.value === 'none' | distance.value > 500 | zip.length < 5 | zip.length > 5) {
+        console.log(`error`);
+    } else {
+        console.log(type.value, distance.value, zip.length);
+        return fetch(`https://api.petfinder.com/v2/oauth2/token`, {
+            method: 'POST',
+            body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(function (resp) {
+            return resp.json();
+        }).then(function (data) {
+            return fetch(`https://api.petfinder.com/v2/animals?type=${type.value}&location=${zip.value}&distance=${distance.value}`, {
+                headers: {
+                    'Authorization': data.token_type + ' ' + data.access_token,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function (resp) {
+                return resp.json()
+            }).then(function (data) {
+                animalData = data.animals
+                console.log(animalData);
+                return animalData
+            })
+        }).catch(function (err) {
+            console.log('something went wrong', err);
+        });
+    }
+})
+// const type1 = 'dog'
+// const zip1 = '55378'
+// const distance1 = 500
+// const dataData = () => {
+//     let animalData
+//     if (type1 !== 'dog' && distance1 > 500) {
 //         console.log(`error`);
 //     } else {
 //         return fetch(`https://api.petfinder.com/v2/oauth2/token`, {
@@ -24,8 +61,8 @@ const distance = document.getElementById('distance')
 //             }
 //         }).then(function (resp) {
 //             return resp.json();
-//         }).then(function (data) {
-//             return fetch(`https://api.petfinder.com/v2/animals?type=${type.value}&location=${zip.value}&distance=${distance.value}`, {
+//         }).then(randomDatas = function (data) {
+//             return fetch(`https://api.petfinder.com/v2/animals?type=${type1}&location=${zip1}&distance=${distance1}`, {
 //                 headers: {
 //                     'Authorization': data.token_type + ' ' + data.access_token,
 //                     'Content-Type': 'application/x-www-form-urlencoded'
@@ -33,24 +70,28 @@ const distance = document.getElementById('distance')
 //             }).then(function (resp) {
 //                 return resp.json()
 //             }).then(function (data) {
-//                 let animalData = data.animals
+//                 animalData = data.animals
 //                 console.log(data.animals);
 //                 return animalData
+//             }).catch(function (err) {
+//                 console.log(`something Failed`);
 //             })
 //         }).catch(function (err) {
 //             console.log('something went wrong', err);
 //         });
-
 //     }
-//     console.log(animalData);
-// })
+
+
+// }
+
+
+// dataData().then(animalData => console.log(animalData))
+
+
 
 var org = 'RI77';
 var status = 'adoptable';
 
-
-// Call the API
-// This is a POST request, because we need the API to generate a new token for us
 const getData = () => {
     let orgData
     return fetch('https://api.petfinder.com/v2/oauth2/token', {
@@ -66,11 +107,6 @@ const getData = () => {
 
     }).then(randomData = function (data) {
 
-        // Log the API data
-        //console.log('token', data);
-
-        // Return a second API call
-        // This one uses the token we received for authentication
         return fetch('https://api.petfinder.com/v2/animals?organization=' + org + '&status=' + status, {
             headers: {
                 'Authorization': data.token_type + ' ' + data.access_token,
@@ -89,44 +125,9 @@ const getData = () => {
         console.log(data);
         return orgData
     })
-    console.log(orgData);
+
 }
-getData().then(orgData => orgData.forEach(animal => {
-    console.log(animal);
-}))
+getData().then(orgData => console.log(orgData))
 
-
-// async function animalData() {
-//     const response = await fetch(`https://api.petfinder.com/v2/animals?type=dog`, {});
-//     const json = await response.json()
-//     console.log(json);
-// }
-
-// fetch(`https://api.petfinder.com/v2/oauth2/token`, {
-//     method: 'POST',
-//     body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded'
-//     }
-// }).then(function (resp) {
-//     // Return the response as JSON
-//     return resp.json();
-//     
-// }).then(function (data) {
-//     return fetch(`https://api.petfinder.com/v2/animals?type=dog`, {
-//         headers: {
-//             'Authorization': data.token_type + ' ' + data.access_token,
-//             'Content-Type': 'application/x-www-form-urlencoded'
-//         }
-//     }).then(function (resp) {
-//         return resp.json()
-//     }).then(function (data) {
-//         console.log(data);
-//     })
-// }).catch(function (err) {
-
-//     // Log any errors
-//     console.log('something went wrong', err);
-
-// });
+//getAnimalData().then(animalData => console.log(animalData))
 
